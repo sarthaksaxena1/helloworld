e82d298a76737aa93b19d1fc0c289b515d1741f9
Sarthak Saxena <blogwithsarthak@gmail.com>
2020-10-02 21:17:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("Hello Neeraj")
 print("Hello Vansh")
 print("Hello Samaksh")
 print("Hello Jatin")
+print("Sarthak Saxena here")
 def fun(name):
   print('Hello {}'.format(name))
 fun('World')
